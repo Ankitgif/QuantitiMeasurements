7c55fec08132522e3d9af3cba002f1b6ae65a90e
Ankitgif <ankitsinha2303@gmail.com>
2020-05-14 17:35:09 +0530

    [Ankit] Added test case to check not equal of 2 inch variables

--- src/test/java/com/quantitymeasurments/QuantityTest.java
+++ src/test/java/com/quantitymeasurments/QuantityTest.java
@@ -23,6 +23,12 @@ public class QuantityTest {
         Inch inch2 = new Inch(0.0);
         Assert.assertEquals(inch1,inch2);
     }
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1,inch2);
+    }
 
 
 }
