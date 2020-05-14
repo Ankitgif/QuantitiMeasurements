c9fc4a3d77f9d6c4b67abe4538b8f07fa4426b7b
Ankitgif <ankitsinha2303@gmail.com>
2020-05-14 18:56:04 +0530

    [Ankit] Added compare method for 2 different variable to compare

--- src/main/java/com/quantitymeasurments/Length.java
+++ src/main/java/com/quantitymeasurments/Length.java
@@ -1,8 +1,9 @@
 package com.quantitymeasurments;
 
-import java.util.Objects;
-
 public class Length {
+
+
+    private static final double FEET_TO_INCH = 12.0;
 
     enum Unit{FEET, INCH};
     private final Unit unit;
@@ -12,6 +13,15 @@ public class Length {
         this.unit = unit;
         this.value = value;
 
+    }
+
+    public boolean compare(Length that) {
+        if(this.unit.equals(that.unit))
+            return this.equals(that);
+        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
+            return Double.compare(this.value*FEET_TO_INCH, that.value) == 0;
+
+        return false;
     }
 
     @Override
