f8763093784e4f838ffe1a71e1694a70092a010a
Ankitgif <ankitsinha2303@gmail.com>
2020-05-15 18:38:03 +0530

    [Ankit] Refactored code, removed if-else

--- src/main/java/com/quantitymeasurments/Length.java
+++ src/main/java/com/quantitymeasurments/Length.java
@@ -2,16 +2,8 @@ package com.quantitymeasurments;
 
 public class Length {
 
-
-    private static final double FEET_TO_INCH = 12.0;
-    private static final double FEET_TO_YARD = 3.0;
-    private static final double YARD_TO_INCH = 36;
-    private static final double INCH_TO_YARD = 36;
-    private static final double YARD_TO_FEET = 3.0;
-
-    enum Unit{FEET, INCH, YARD};
-    private final Unit unit;
-    private final double value;
+      public final Unit unit;
+      public final double value;
 
     public Length(Unit unit, double value) {
         this.unit = unit;
@@ -20,23 +12,7 @@ public class Length {
     }
 
     public boolean compare(Length that) {
-        if(this.unit.equals(Unit.FEET) && that.unit.equals(Unit.FEET))
-            return Double.compare(this.value,that.value) == 0;
-        if(this.unit.equals((Unit.INCH)) && that.unit.equals(Unit.INCH))
-            return Double.compare(this.value,that.value) == 0;
-        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
-            return Double.compare(this.value*FEET_TO_INCH, that.value) == 0;
-        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.YARD))
-            return Double.compare(this.value/FEET_TO_YARD, that.value) == 0;
-        if (this.unit.equals(Unit.YARD) && that.unit.equals(Unit.INCH))
-            return Double.compare(this.value*YARD_TO_INCH, that.value) == 0;
-        if (this.unit.equals(Unit.INCH) && that.unit.equals(Unit.YARD))
-            return Double.compare(this.value/INCH_TO_YARD, that.value) == 0;
-        if (this.unit.equals(Unit.YARD) && that.unit.equals(Unit.FEET))
-            return Double.compare(this.value*YARD_TO_FEET, that.value) == 0;
-
-
-        return false;
+        return this.unit.compare(this,that);
     }
 
     @Override
@@ -47,6 +23,5 @@ public class Length {
         return Double.compare(length.value, value) == 0 &&
                 unit == length.unit;
     }
-
 
 }
