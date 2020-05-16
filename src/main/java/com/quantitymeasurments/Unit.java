3ba00b63232b177bbcb972a84e6820bc69da0dd6
Ankitgif <ankitsinha2303@gmail.com>
2020-05-16 13:57:38 +0530

    [Ankit] Added base unit conversion of Tonne

--- src/main/java/com/quantitymeasurments/Unit.java
+++ src/main/java/com/quantitymeasurments/Unit.java
@@ -4,7 +4,7 @@ public enum Unit {
 
     FEET(12.0), INCH(1), YARD(36), CENTIMETER(0.4),
     LITRE(1),GALLON(3.78),MILLILITRE(0.001),KILOGRAM(1000),
-    GRAM(1);
+    GRAM(1),TONNE(1000000);
 
     private double baseUnitConversion;
 
