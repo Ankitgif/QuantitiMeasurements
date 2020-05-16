ffc81281660182ae05162166703fccb64542caad
Ankitgif <ankitsinha2303@gmail.com>
2020-05-16 14:22:01 +0530

    [Ankit] Refactored base unit conversion of Kilogram, gram and tonne

--- src/main/java/com/quantitymeasurments/Unit.java
+++ src/main/java/com/quantitymeasurments/Unit.java
@@ -3,8 +3,8 @@ package com.quantitymeasurments;
 public enum Unit {
 
     FEET(12.0), INCH(1), YARD(36), CENTIMETER(0.4),
-    LITRE(1),GALLON(3.78),MILLILITRE(0.001),KILOGRAM(1000),
-    GRAM(1),TONNE(1000000);
+    LITRE(1),GALLON(3.78),MILLILITRE(0.001),KILOGRAM(1),
+    GRAM(0.001),TONNE(1000);
 
     private double baseUnitConversion;
 
