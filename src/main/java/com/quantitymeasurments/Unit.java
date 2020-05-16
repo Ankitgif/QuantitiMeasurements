48c3a6c93745ed8ddab1998469a71540cae93d72
Ankitgif <ankitsinha2303@gmail.com>
2020-05-16 13:53:14 +0530

    [Ankit] Added base unit conversion of Kilogram and gram

--- src/main/java/com/quantitymeasurments/Unit.java
+++ src/main/java/com/quantitymeasurments/Unit.java
@@ -3,7 +3,8 @@ package com.quantitymeasurments;
 public enum Unit {
 
     FEET(12.0), INCH(1), YARD(36), CENTIMETER(0.4),
-    LITRE(1),GALLON(3.78),MILLILITRE(0.001);
+    LITRE(1),GALLON(3.78),MILLILITRE(0.001),KILOGRAM(1000),
+    GRAM(1);
 
     private double baseUnitConversion;
 
