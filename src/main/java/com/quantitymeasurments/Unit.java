e66a1119ae075e091ccc64acb9362a3abb128245
Ankitgif <ankitsinha2303@gmail.com>
2020-05-16 14:55:39 +0530

    [Ankit] Added base unit conversion of fahrenheit and celsius

--- src/main/java/com/quantitymeasurments/Unit.java
+++ src/main/java/com/quantitymeasurments/Unit.java
@@ -4,7 +4,7 @@ public enum Unit {
 
     FEET(12.0), INCH(1), YARD(36), CENTIMETER(0.4),
     LITRE(1),GALLON(3.78),MILLILITRE(0.001),KILOGRAM(1),
-    GRAM(0.001),TONNE(1000);
+    GRAM(0.001),TONNE(1000),FAHRENHEIT(1),CELSIUS(2.12);
 
     private double baseUnitConversion;
 
