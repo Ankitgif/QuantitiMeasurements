6943c85ab8aec3aa20b690ba4314b098470ac5b5
Ankitgif <ankitsinha2303@gmail.com>
2020-05-20 23:54:06 +0530

    [Ankit] Refactored code

--- src/main/java/com/quantitymeasurments/Unit.java
+++ src/main/java/com/quantitymeasurments/Unit.java
@@ -3,8 +3,8 @@ package com.quantitymeasurments;
 public enum Unit {
 
     FEET(12.0), INCH(1), YARD(36), CENTIMETER(0.4),
-    LITRE(1),GALLON(3.78),MILLILITRE(0.001),KILOGRAM(1),
-    GRAM(0.001),TONNE(1000),FAHRENHEIT(1),CELSIUS(2.12);
+    LITRE(1), GALLON(3.78), MILLILITRE(0.001), KILOGRAM(1),
+    GRAM(0.001), TONNE(1000), FAHRENHEIT(1), CELSIUS(2.12);
 
     private double baseUnitConversion;
 
