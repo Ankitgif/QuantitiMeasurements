c503499a57479c582ca14e68a4e42a1d7de88b27
Ankitgif <ankitsinha2303@gmail.com>
2020-05-20 23:53:21 +0530

    [Ankit] Refactored code as DRY was violated

--- src/main/java/com/quantitymeasurments/Quantity.java
+++ src/main/java/com/quantitymeasurments/Quantity.java
@@ -8,26 +8,18 @@ public class Quantity {
     public Quantity(Unit unit, double value) {
         this.unit = unit;
         this.value = value;
-
     }
 
-    public static double addQuantity(Quantity quantity1, Quantity quantity2) {
-         double addResult = quantity1.unit.getConvertedValue(quantity1.value) + quantity2.unit.getConvertedValue(quantity2.value);
-         return addResult;
-    }
-
-    public boolean compare(Quantity that) {
-        return Double.compare(that.unit.getConvertedValue(that.value),
-                this.unit.getConvertedValue(this.value)) == 0;
+    public Quantity addQuantity(Quantity other) {
+        return new Quantity(Unit.KILOGRAM, this.unit.getConvertedValue(this.value) + other.unit.getConvertedValue(other.value));
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Quantity quantity = (Quantity) o;
-        return Double.compare(quantity.value, value) == 0 &&
-                unit == quantity.unit;
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Quantity that = (Quantity) other;
+        return Double.compare(that.unit.getConvertedValue(that.value),
+                this.unit.getConvertedValue(this.value)) == 0;
     }
-
 }
